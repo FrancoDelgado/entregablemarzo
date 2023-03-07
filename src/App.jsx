5288043b6cd902ca0fa012a4666a72e8fdff19da
Franco <francodocpp@gmail.com>
2023-03-07 16:51:14 -0300

    segundo commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,65 @@
+import { useState } from "react";
+import Card from "./Card";
+
 function App() {
   //Aqui deberias agregar los estados y los handlers para los inputs
 
+  const form = () => {
+    const [auto, setauto] = useState({
+      marcaAuto: '',
+      modelo: '',
+    })
+  }
+
+  const [mostrar, setmostrar] = useState(true)
+  
+  const handleSumbit = (e) => {
+    if (auto.marcaAuto.length > 4 &&
+        modelo.length > 7
+        ){
+      setmostrar(true);
+
+    }else {setmostrar(false)}
+  }
+
+
   return (
     <div className="App">
-      <h1>Elige un color</h1>
-      <form>{/* aqui deberias escribir tu codigo */}</form>
+      <h1>Elige un Auto</h1>
+      
+      <form onSubmit={handleSumbit}>
+      <label>Marca del Auto</label>
+      <input
+        type = "text"
+        value = {auto.marcaAuto}
+        onChange = {(e) => setauto({...auto, marcaAuto: e.target.value})}
+      />
+
+
+
+      <label>Modelo del Auto</label>
+      <input
+        type = "text"
+        value = {auto.modelo}
+        onChange = {(e) => setauto({...auto, modelo: e.target.value})}
+      />
+      
+
+      <button type="submit">ENVIAR</button>
+      </form>
+      
+      {mostrar ?  (
+        <>
+        <Card/>
+        </>
+      ) : <h3>Por favor chequea que la información sea correcta</h3> }
     </div>
+  
+  
+  
+  
+  
+  
   );
 }
 
--- src/Card.jsx
+++ src/Card.jsx
@@ -1,10 +1,12 @@
 //Este componente deberia recibir por props y mostrar en pantalla la informacion
 //que envia el usuario
 
-function Card() {
+function Card(props) {
+  
   return (
     <div>
-      <h2>Esto es un componente</h2>
+      <h2>{props.auto.marcaAuto}</h2>
+      <h2>{props.auto.modelo}</h2>
     </div>
   );
 }
